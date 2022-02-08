839069ae2bfd3fe100fdaa8e4065a6ed384982e0
46kuri_ <ghost.46kuri@gmail.com>
2022-02-08 22:22:17 +0900

    [Add] サーバー名の識別処理追加

--- src/main/java/com/github/sirokuri_/jgserverwarp/JgServerWarp.java
+++ src/main/java/com/github/sirokuri_/jgserverwarp/JgServerWarp.java
@@ -5,6 +5,7 @@ import com.github.sirokuri_.jgserverwarp.listeners.JgServerWarpInteract;
 import com.github.sirokuri_.jgserverwarp.listeners.JgServerWarpJoin;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,6 +21,7 @@ public final class JgServerWarp extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new JgServerWarpJoin(this), this);
         getServer().getPluginManager().registerEvents(new JgServerWarpInteract(this), this);
         getCommand("JgServerWarp").setExecutor(new JgServerWarpCommand(this));
+        saveDefaultConfig();
     }
 
     @Override
@@ -32,12 +34,28 @@ public final class JgServerWarp extends JavaPlugin {
         ItemMeta itemMeta = itemStack.getItemMeta();
         itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&ajg内でサーバ移動をする"));
         List<String> lore = new ArrayList<String>();
-        lore.add(ChatColor.translateAlternateColorCodes('&', "&d左クリックでjgに"));
-        lore.add(ChatColor.translateAlternateColorCodes('&', "&d右クリックでjg1にサーバー移動ができます"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&d右クリックでjgかjg1にサーバー移動ができます"));
         lore.add(ChatColor.translateAlternateColorCodes('&', "&dこのアイテムを無くした場合は"));
         lore.add(ChatColor.translateAlternateColorCodes('&', "&dspawn地点のウィザースケルトンの頭を触ってください"));
         itemMeta.setLore(lore);
         itemStack.setItemMeta(itemMeta);
         return itemStack;
+    }
+
+    private FileConfiguration config = null;
+
+    public FileConfiguration config(){
+        // ロードする
+        load();
+        return config;
+    }
+
+    public void load() {
+        // 設定ファイルを保存
+        saveDefaultConfig();
+        if (config != null) {
+            reloadConfig();
+        }
+        config = getConfig();
     }
 }
--- src/main/java/com/github/sirokuri_/jgserverwarp/listeners/JgServerWarpInteract.java
+++ src/main/java/com/github/sirokuri_/jgserverwarp/listeners/JgServerWarpInteract.java
@@ -25,17 +25,25 @@ public class JgServerWarpInteract implements Listener{
         Player player = event.getPlayer();
         ItemStack itemStack = player.getInventory().getItemInMainHand();
         if ((event.getHand() != EquipmentSlot.HAND)) return;
+        String config = plugin.config().getString("serverName");
+        if (config == null || config.equals("Default")){
+            player.sendMessage("正しくサーバー名が指定されていません。運営へ報告しましょう");
+            return;
+        }
         ItemMeta itemMeta = itemStack.getItemMeta();
         if (itemMeta == null) return;
         if (itemStack.getType() == Material.PAPER){
             if (itemMeta.getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&ajg内でサーバ移動をする"))){
-                if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK){
-                    player.performCommand("advancedportals warp jg");
-                    plugin.getLogger().info(player.getName() + " issued server command: /advancedportals warp jg");
-                }
-                if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    player.performCommand("advancedportals warp jg1");
-                    plugin.getLogger().info(player.getName() + " issued server command: /advancedportals warp jg1");
+                if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK){
+                    if (config.equals("jg")){
+                        player.performCommand("advancedPortals warp jg1");
+                        plugin.getLogger().info(player.getName() + " issued server command: /advancedPortals warp jg1");
+                    }else if (config.equals("jg1")) {
+                        player.performCommand("advancedPortals warp jg");
+                        plugin.getLogger().info(player.getName() + " issued server command: /advancedPortals warp jg");
+                    } else {
+                        player.sendMessage("正しくサーバー名が指定されていません。運営へ報告しましょう");
+                    }
                 }
             }
         }
